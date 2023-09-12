abee4b1f2b85d3c4613ce190d25838396b60c39b
Piet Helzel <piet.helzel@outlook.com>
2023-09-13 01:19:38 +0200

    Change "requires" to "dependencies" (stupid mistake)

--- dot_config/nvim/lua/plugins/nvim-tree.lua
+++ dot_config/nvim/lua/plugins/nvim-tree.lua
@@ -1,13 +1,12 @@
 return {
     "nvim-tree/nvim-tree.lua",
-    requires = {
+    name = "nvim-tree",
+    dependencies = {
         "nvim-tree/nvim-web-devicons"
     },
     keys = {
         {mode = "n", "<leader>e", ":NvimTreeFocus<cr>"},
         {mode = "n", "<leader><S-e>", ":NvimTreeToggle<cr>"}
     },
-    config = function()
-        require("nvim-tree").setup()
-    end
+    opts = {}
 }
