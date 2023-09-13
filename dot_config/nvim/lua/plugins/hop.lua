459b41144e76355e537db120b3aa8b91ef537bc8
Piet Helzel <piet.helzel@outlook.com>
2023-09-13 16:32:16 +0200

    Add hop and comment plugins

---
+++ dot_config/nvim/lua/plugins/hop.lua
@@ -0,0 +1,10 @@
+return {
+    "smoka7/hop.nvim",
+    opts = {},
+    keys = {
+        {mode = "n", "<leader>hh", ":HopWord<cr>"},
+        {mode = "n", "<leader>hl", ":HopLine<cr>"},
+        {mode = "n", "<leader>hs", ":HopeLineStart<cr>"},
+        {mode = "n", "<leader>hn", ":HopNodes<cr>"}
+    }
+}
