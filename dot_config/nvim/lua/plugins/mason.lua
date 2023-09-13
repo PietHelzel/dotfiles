6dbd193089954de7dce9d376e038b7897cb9ccde
Piet Helzel <piet.helzel@outlook.com>
2023-09-13 21:46:25 +0200

    Basic lsp setup

---
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,19 @@
+return {
+    "williamboman/mason.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    },
+    lazy = false,
+    config = function()
+        local lspconfig = require("lspconfig")
+        require("mason").setup()
+        
+        require("mason-lspconfig").setup({})
+        require("mason-lspconfig").setup_handlers({
+            function(server_name)
+                lspconfig[server_name].setup({})
+            end
+        })
+    end
+}
