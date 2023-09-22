7a7067c400488f1a7d27a65e2ce72ba44a9b1cb5
Piet Helzel <piet.helzel@outlook.com>
2023-09-22 13:20:35 +0200

    Better hop keybinds

--- dot_config/nvim/lua/plugins/hop.lua
+++ dot_config/nvim/lua/plugins/hop.lua
@@ -2,9 +2,9 @@ return {
     "smoka7/hop.nvim",
     opts = {},
     keys = {
-        {mode = "n", "<leader>hh", ":HopWord<cr>"},
-        {mode = "n", "<leader>hl", ":HopLine<cr>"},
-        {mode = "n", "<leader>hs", ":HopeLineStart<cr>"},
-        {mode = "n", "<leader>hn", ":HopNodes<cr>"}
+        {mode = "n", "<leader>h", ":HopWord<cr>"},
+        {mode = "n", "<leader>jl", ":HopLine<cr>"},
+        {mode = "n", "<leader>js", ":HopeLineStart<cr>"},
+        {mode = "n", "<leader>jn", ":HopNodes<cr>"}
     }
 }
