56deb0a83ecc925faf0e886e57b51d7a691e6d99
Piet Helzel <piet.helzel@outlook.com>
2023-10-05 11:31:16 +0200

    Use opts instead of config function

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,15 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function ()
+    opts = {
         require("nvim-treesitter.configs").setup({
             auto_install = true,
             highlight = {
                 enable = true
+            },
+            indent = {
+                enable = true
             }
         })
-    end
+    }
 }
