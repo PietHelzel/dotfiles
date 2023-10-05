92181789dbb3cd612cd6c8c293be719263efd93f
Piet Helzel <piet.helzel@outlook.com>
2023-10-05 11:31:34 +0200

    Add lsp-progress indicator to lualine

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,12 +1,22 @@
 return {
     "nvim-lualine/lualine.nvim",
     dependencies = {
-        "nvim-tree/nvim-web-devicons"
+        "nvim-tree/nvim-web-devicons",
+        "WhoIsSethDaniel/lualine-lsp-progress.nvim"
     },
     opts = {
         options = {
             component_separators = { left = '|', right = '|'},
             section_separators = { left = '', right = ''},
+        },
+        sections = {
+            lualine_c = {
+                "filename",
+                {
+                    "lsp_progress",
+                    spinner_symbols = { '🌑 ', '🌒 ', '🌓 ', '🌔 ', '🌕 ', '🌖 ', '🌗 ', '🌘 ' }
+                }
+            }
         }
-    }
+    },
 }
