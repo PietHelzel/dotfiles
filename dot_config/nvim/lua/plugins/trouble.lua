a077099721c245d4df7ea72bf07b45a6dc835b9c
Piet Helzel <piet.helzel@outlook.com>
2023-10-10 18:43:48 +0200

    Add trouble plugin

---
+++ dot_config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,15 @@
+return {
+    "folke/trouble.nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    opts = {},
+    keys = {
+        {mode = "n", "<leader>tx", ":TroubleToggle<cr>", desc = "Toggle View"},
+        {mode = "n", "<leader>tw", ":Trouble workspace_diagnostics<cr>", desc = "Workspace Diagnostics"},
+        {mode = "n", "<leader>td", ":Trouble document_diagnostics<cr>", desc = "Document Diagnostics"},
+        {mode = "n", "<leader>tr", ":Trouble lsp_references<cr>", desc = "List References"},
+        {mode = "n", "<leader>tf", ":Trouble lsp_definitions<cr>", desc = "List Definitons"},
+        {mode = "n", "<leader>ti", ":Trouble lsp_implementations<cr>", desc = "List Implementations"}
+    }
+}
