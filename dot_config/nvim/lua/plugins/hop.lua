fe63e392edc74f6d682c98dc453a2eadbe75c20d
Piet Helzel <piet.helzel@outlook.com>
2023-10-24 14:58:16 +0200

    Better keybindings for Hop

--- dot_config/nvim/lua/plugins/hop.lua
+++ dot_config/nvim/lua/plugins/hop.lua
@@ -2,9 +2,18 @@ return {
     "smoka7/hop.nvim",
     opts = {},
     keys = {
-        {mode = "n", "<leader>h", ":HopWord<cr>", desc = "hop to word"},
-        {mode = "n", "<leader>jl", ":HopLine<cr>", desc = "hop to line"},
-        {mode = "n", "<leader>js", ":HopeLineStart<cr>", desc = "hop to line start"},
-        {mode = "n", "<leader>jn", ":HopNodes<cr>", desc = "hop to treesitter nodes"}
+        {mode = {"n", "v"}, "<leader>h", function () require("hop").hint_words() end, desc = "hop to word"},
+        {mode = {"n", "v"}, "<leader>jl", function () require("hop").hint_lines() end, desc = "hop to line"},
+        {mode = {"n", "v"}, "<leader>js", function () require("hop").hint_lines_skip_whitespace() end, desc = "hop to line start"},
+        {mode = {"n", "v"}, "<leader>jn", function () require("hop").hint_nodes() end, desc = "hop to treesitter nodes"},
+        {mode = {"n", "v"}, "<leader>jc", function () require("hop").hint_char1() end, desc = "hop to character"},
+        {mode = {"n", "v"}, "<leader>je",
+            function ()
+                require("hop").hint_words({
+                    hint_position = require("hop.hint").HintPosition.END
+                })
+            end,
+        desc = "hop to word end"
+        }
     }
 }
