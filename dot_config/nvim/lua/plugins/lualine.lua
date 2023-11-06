d7fbf3c624a430f065dbc0e14259a9a357d8f832
Piet Helzel <piet.helzel@outlook.com>
2023-11-06 13:13:25 +0100

    Remove lsp progress indicator

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -2,21 +2,11 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = {
         "nvim-tree/nvim-web-devicons",
-        "WhoIsSethDaniel/lualine-lsp-progress.nvim"
     },
     opts = {
         options = {
             component_separators = { left = '|', right = '|'},
             section_separators = { left = '', right = ''},
-        },
-        sections = {
-            lualine_c = {
-                "filename",
-                {
-                    "lsp_progress",
-                    spinner_symbols = { '🌑 ', '🌒 ', '🌓 ', '🌔 ', '🌕 ', '🌖 ', '🌗 ', '🌘 ' }
-                }
-            }
         }
-    },
+    }
 }
