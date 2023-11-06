be97104c1710e8d4043675f1c628d6e0f3e3199d
Piet Helzel <piet.helzel@outlook.com>
2023-11-06 13:12:53 +0100

    Add bufferline instead of barbar because of bug

---
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,45 @@
+return {
+    "akinsho/bufferline.nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+        {
+            "echasnovski/mini.bufremove",
+            opts = {}
+        }
+    },
+    lazy = false,
+    config = function ()
+        require("bufferline").setup({
+            options = {
+                close_command = function(n) require("mini.bufremove").delete(n, false) end,
+                right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+                groups = {
+                    items = {
+                        require('bufferline.groups').builtin.pinned:with({ icon = "󰐃" })
+                    }
+                },
+                offsets = {{
+                    filetype = "NvimTree",
+                    text = " File Explorer",
+                    highlight = "Directory",
+                    separator = false,
+                }},
+            }
+        })
+    end,
+    keys = {
+        {mode = "n", "<leader>q", function() require("mini.bufremove").delete(0, false) end, desc = "close current buffer"},
+        {mode = "n", "<leader><S-q>", function() require("mini.bufremove").delete(0, true) end, desc = "close current buffer (force)"},
+        {mode = "n", "<A-j>", "<cmd>BufferLineCyclePrev<cr>", desc = "goto prev buffer"},
+        {mode = "n", "<A-k>", "<cmd>BufferLineCycleNext<cr>", desc = "goto next buffer"},
+        {mode = "n", "<A-h>", ":BufferLineMovePrev<cr>", desc = "move buffer left"},
+        {mode = "n", "<A-l>", ":BufferLineMoveNext<cr>", desc = "move buffer right"},
+        {mode = "n", "<leader>bb", ":BufferLinePick<cr>", desc = "pick buffer"},
+        {mode = "n", "<leader>bd", ":BufferLinePickClose<cr>", desc = "pick buffer + close"},
+        {mode = "n", "<leader>bp", ":BufferLineTogglePin<cr>", desc = "pin buffer"},
+        {mode = "n", "<leader>bw", ":BufferLineGroupClose ungrouped<cr>", desc = "close unpinned buffers"},
+        {mode = "n", "<leader>bo", ":BufferLineCloseOthers<cr>", desc = "close others"},
+        {mode = "n", "<leader>bj", ":BufferLineCloseLeft<cr>", desc = "close buffers left"},
+        {mode = "n", "<leader>bk", ":BufferLineCloseRight<cr>", desc = "close buffers right"}
+    }
+}
