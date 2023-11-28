19809d0fdccba4c47a2d230c2272e8a5f2f853cf
Piet Helzel <piet.helzel@outlook.com>
2023-11-28 17:14:38 +0100

    Hide tabline when zen-mode is activated

--- dot_config/nvim/lua/plugins/zen-mode.lua
+++ dot_config/nvim/lua/plugins/zen-mode.lua
@@ -4,7 +4,13 @@ return {
         window = {
             backdrop = 0.8,
             width = 0.7
-        }
+        },
+        on_open = function (_)
+            vim.opt.showtabline = 0
+        end,
+        on_close = function (_)
+            vim.opt.showtabline = 2
+        end
     },
     keys = {
         {mode = "n", "<leader>z", "<cmd>ZenMode<cr>", desc = "toggle zen mode"}
